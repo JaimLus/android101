fe9e5445e9bb61162de7072b11bde1e9d178ed8d
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2015-04-20 22:12:23 -0500

    Material Design

--- SimpleTodo/app/src/main/java/com/carlosrojasblog/simpletodo/MainActivity.java
+++ SimpleTodo/app/src/main/java/com/carlosrojasblog/simpletodo/MainActivity.java
@@ -2,6 +2,7 @@ package com.carlosrojasblog.simpletodo;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,7 +19,6 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
-
     private ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
     private ListView lvItems;
@@ -27,15 +27,23 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // ADD HERE
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_my_toolbar);
+
+
+
         lvItems = (ListView) findViewById(R.id.lvItems);
         items = new ArrayList<String>();
         readItems();
         itemsAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
+        //items.add("First Item");
+        //items.add("Second Item");
 
         setupListViewListener();
+        toolbar.setTitle("TodoApp");
+        setSupportActionBar(toolbar);
+
     }
 
     // Attaches a long click listener to the listview
@@ -58,20 +66,20 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
     public void onAddItem(View v) {
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
         itemsAdapter.add(itemText);
         etNewItem.setText("");
         writeItems();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     @Override
